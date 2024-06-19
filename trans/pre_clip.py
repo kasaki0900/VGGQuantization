8b5c2a6ca72ee14d0e79441f764d75e4a249f465
kasaki0900 <2563576347@qq.com>
2024-06-19 20:43:03 +0800

    update

---
+++ trans/pre_clip.py
@@ -0,0 +1,17 @@
+import torch
+
+
+def clip_cut(pth_path, threshold_percent):
+    """original model"""
+    params = torch.load(pth_path)
+    for name, param in params.items():
+        if not param.shape == torch.Size([]):
+            low_value = torch.quantile(param, threshold_percent / 100)
+            high_value = torch.quantile(param, 1 - (threshold_percent / 100))
+            params[name] = torch.clamp(param, max=high_value, min=low_value)
+
+    return params
+
+
+if __name__ == '__main__':
+    pass
